e9496a86e060029ea8829167dac84ba55a6e22e0
logoraz <erikalmaraz@fastmail.com>
2024-06-24 20:14:38 -0700

    Update comments in main.

--- source/main.lisp
+++ source/main.lisp
@@ -4,3 +4,8 @@
 (in-package :chemixir)
 
 ;; blah blah blah.
+
+
+;; This was created using cl-project, see below:
+;; (ql:quickload "cl-project")
+;; (cl-project:make-project #P"./path-to-project/root/")
