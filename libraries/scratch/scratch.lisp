7f298186846bd8963a7db789e3c3992f03a5794a
logoraz <erikalmaraz@fastmail.com>
2024-08-09 07:24:18 -0700

    Cleanup scratch.

--- libraries/scratch/scratch.lisp
+++ libraries/scratch/scratch.lisp
@@ -7,7 +7,7 @@
         :repeat limit
         :collect a))
 
-;;; Example - Functional Calculator
+;;; Example - Functional Calculator in Common Lisp
 (defun make-calculator (current-value)
   #'(lambda (operation)
       (setf current-value (funcall operation current-value))))
@@ -57,33 +57,3 @@
 
 (defun (setf current-theme) (theme)
   (setf *current-theme* theme))
-
-;;; Definiing Systems
-
-;; A system is a collection of Lisp files that together constitute an application or a
-;; library, and that should therefore be managed as a whole. A system definition
-;; describes which source files make up the system, what the dependencies among them are,
-;; and the order they should be compiled and loaded in.
-
-;; ASDF is the standard build system for Common Lisp. It is shipped in most Common Lisp
-;; implementations. It includes UIOP, “the Utilities for Implementation- and
-;; OS- Portability”.
-
-;; Using quicklisp -> issue with Guix finding micros...
-;; See:  https://www.quicklisp.org/beta/
-
-;; See Advanced Dependencies Management
-;; -> https://lispcookbook.github.io/cl-cookbook/getting-started.html
-;;    ASDF and Quicklisp loadable directories:
-;;   |--> ~/common-lisp/
-;;   |--> ~/quicklisp/local-projects/
-;;   |--> ~/.local/share/common-lisp/source/
-;;
-;; A library installed here is automatically available for every project.
-
-;; Quicklisp Initialization
-;; Load quicklisp - not sure why it isn't being loaded in ~/.sbclrc file...
-(let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
-                                       (user-homedir-pathname))))
-  (when (probe-file quicklisp-init)
-    (load quicklisp-init)))
